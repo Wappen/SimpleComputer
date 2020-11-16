d11e6472ce52c08a60509369e980a4d633fab79f
Lenz Koxholt <tlinden110@gmail.com>
2020-11-16 16:32:43 +0100

    Sorting

--- SimpleComputer/Processors/SimpleProcessor.cs
+++ SimpleComputer/Processors/SimpleProcessor.cs
@@ -9,38 +9,6 @@ namespace SimpleComputer.Processors
     [Instruction(typeof(ExitInstruction))]
     public class SimpleProcessor : Processor
     {
-        protected override void PrintProgram()
-        {
-            for (int i = 0; i < Program.Length; i++)
-            {
-                Console.Write($"{i}");
-                Console.CursorLeft = 4;
-                char lc = (ProgramCounter == i) ? '>' : '|';
-                char rc = (ProgramCounter == i) ? '<' : ' ';
-                Console.Write($"{lc} {Program[i]} {rc}");
-                Console.WriteLine();
-            }
-        }
-
-        protected override void PrintMemory()
-        {
-            for (int i = 0; i < Memory.Length; i++)
-            {
-                Console.Write($"{i}");
-                Console.CursorLeft = 4;
-                Console.Write($"| {Memory[i]}");
-                Console.WriteLine();
-            }
-        }
-
-        protected override void PrintOutput()
-        {
-            foreach (var line in Output)
-            {
-                Console.WriteLine(line);
-            }
-        }
-
         [Name("+")]
         public class IncInstruction : Instruction
         {
@@ -100,6 +68,38 @@ namespace SimpleComputer.Processors
             public override bool Execute()
             {
                 return false;
+            }
+        }
+
+        protected override void PrintProgram()
+        {
+            for (int i = 0; i < Program.Length; i++)
+            {
+                Console.Write($"{i}");
+                Console.CursorLeft = 4;
+                char lc = (ProgramCounter == i) ? '>' : '|';
+                char rc = (ProgramCounter == i) ? '<' : ' ';
+                Console.Write($"{lc} {Program[i]} {rc}");
+                Console.WriteLine();
+            }
+        }
+
+        protected override void PrintMemory()
+        {
+            for (int i = 0; i < Memory.Length; i++)
+            {
+                Console.Write($"{i}");
+                Console.CursorLeft = 4;
+                Console.Write($"| {Memory[i]}");
+                Console.WriteLine();
+            }
+        }
+
+        protected override void PrintOutput()
+        {
+            foreach (var line in Output)
+            {
+                Console.WriteLine(line);
             }
         }
     }
